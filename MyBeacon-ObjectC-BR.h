1b0e070dd8f005802f6ed164554c70a18081d1e8
ahua <ahua.chen@gmail.com>
2015-07-01 12:00:25 +0800

    init

---
+++ MyBeacon-ObjectC-BR.h
@@ -0,0 +1,13 @@
+//
+//  MyBeacon-ObjectC-BR.h
+//  MyBeacon
+//
+//  Created by ahua on 15/6/30.
+//  Copyright (c) 2015年 ahua. All rights reserved.
+//
+
+#ifndef MyBeacon_MyBeacon_ObjectC_BR_h
+#define MyBeacon_MyBeacon_ObjectC_BR_h
+
+
+#endif
---
+++ MyBeacon/TrackViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TrackViewController.swift
+//  MyBeacon
+//
+//  Created by ahua on 15/6/30.
+//  Copyright (c) 2015年 ahua. All rights reserved.
+//
+
+import Foundation
